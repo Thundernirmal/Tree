ee222566d01104b0f2e30ac39fce63a3029e90ea
Nirmal Katariya <42312086+Thundernirmal@users.noreply.github.com>
2019-10-01 09:43:39 +0530

    Final

--- B.c
+++ B.c
@@ -3,6 +3,28 @@
 int dp[1111];
 int a[1111];int b[1111];
 
+void insertionSort(int A[],int B[], int size) 
+{ 
+int i, key, j,key2; 
+for (i = 1; i < size; i++) 
+{ 
+	key = A[i]; key2=B[i];
+	j = i-1;
+	while (j >= 0 && A[j] > key) 
+	{ 
+		A[j+1] = A[j]; 
+		B[j+1]=[j];
+		j = j-1; 
+	} 
+	A[j+1] = key; 
+	B[j+1] = key2;
+} 
+}
+int max(int a, int b){
+ if(a > b)
+return a;
+return b;
+}												
 int main()
 {
     #ifndef ONLINE_JUDGE
@@ -22,6 +44,7 @@ int main()
          scanf("%d",b[i]);
      }
      int maxi=0;
+     insertionSort(b,a,N);
       for(int i=0;i<N;i++)
       {
         dp[i]=1;
